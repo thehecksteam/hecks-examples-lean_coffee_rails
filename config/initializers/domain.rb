9cabbe2aa9543762366c4f1fbacd6a2a1eda28ef
Chris Young <beesucker@gmail.com>
2019-09-21 19:16:38 -0700

    show schema on the domain schema show page

--- config/initializers/domain.rb
+++ config/initializers/domain.rb
@@ -2,6 +2,6 @@ require 'hecks-adapters/dynamodb'
 Domain = HecksApp::ApplicationPort
 Domain.config do
   domain LeanCoffee
-  driven_adapter :Dynamodb
+  adapter :Dynamodb
 end
 Hecks::ActiveModel.decorate
